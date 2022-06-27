cad2e539721e2d459f04321a6b93e91b8ca4ef06
Pedro-Alcantara-M <pedrinrn@gmail.com>
2022-06-26 21:45:40 -0300

    lesson 5

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -2,48 +2,21 @@ import { DefaultUi, Player, Youtube } from "@vime/react"
 import { CaretRight, DiscordLogo, FileArrowDown, ImageSquare, Lightning } from "phosphor-react"
 
 import "@vime/core/themes/default.css"
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-    query GetLessonBySlug ($slug: String) {
-    lesson(where: {slug: $slug}) {
-        title
-        videoId
-        description
-        teacher {
-        avatarURL
-        bio
-        name
-        }
-    }
-}`
-
-interface GetLessonBySlugResponse {
-    lesson:{
-        title: string;
-        videoId: string;
-        description: string;
-        teacher: {
-            bio: string;
-            avatarURL: string;
-            name: string;
-        }
-    }
-}
-
+import { useGetLessonBySlugQuery } from "../graphql/types";
 
 interface VideoProps {
     lessonSlug: string;
 }
 
 export const Video = (props: VideoProps) => {
-    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
-        variables:{
+    const { data } = useGetLessonBySlugQuery({
+        variables: {
             slug: props.lessonSlug
         }
     })
+        
 
-    if(!data) {
+    if(!data || !data.lesson) {
         return (
             <div className="flex flex-1">
                 <p>Carregando...</p>
@@ -56,7 +29,7 @@ export const Video = (props: VideoProps) => {
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId={data.lesson.videoId}/>
+                        <Youtube videoId={data.lesson.videoId} key={data.lesson.videoId}/>
                         <DefaultUi />
                     </Player>
                 </div>
@@ -72,18 +45,21 @@ export const Video = (props: VideoProps) => {
                         {data.lesson.description}
                         </p>
 
-                        <div className="flex items-center gap-4 mt-6">
-                            <img 
-                                src={data.lesson.teacher.avatarURL}
-                                alt=""
-                                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                            />
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
+                                <img 
+                                    src={data.lesson.teacher.avatarURL}
+                                    alt=""
+                                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                />
 
-                            <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                    <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                                </div>
                             </div>
-                        </div>
+                        )}
+
                     </div>
 
                     <div className="flex flex-col gap-4 ">
